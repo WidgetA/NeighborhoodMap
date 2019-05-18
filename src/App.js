236f69ba673ba88b2e862d63ecd5062e738c08ac
WidgetA <421413836@qq.com>
2019-05-18 23:26:21 +0800

    addmap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{ lat: 39.975836, lng: 116.318335 }}
+  >
+    <Marker
+      position={{ lat: 39.975836, lng: 116.318335 }}
+    />
+  </GoogleMap>
+));
+
+
+class MapApp extends React.Component {
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <MapWithAMarker
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAO2Qc8-Ef1roxaLlccqGTfNLkTUGJsqMQ&libraries=geometry,drawing,places"
+          loadingElement={<div id="loading"/>}
+          containerElement={<div id="container"/>}
+          mapElement={<div id="map"/>}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MapApp;
