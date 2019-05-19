d66781f0ea610bbd9d14d3bf70496e70a4ebdd0c
WidgetA <421413836@qq.com>
2019-05-19 20:13:40 +0800

    pages update

--- src/App.js
+++ src/App.js
@@ -11,10 +11,22 @@ import {
 const MapWithAMarker = withScriptjs(withGoogleMap(props =>
   <GoogleMap
     defaultZoom={13}
-    defaultCenter={{ lat: 39.975836, lng: 116.318335 }}
+    defaultCenter={{ lat: 39.970008, lng: 116.325888 }}
   >
     <Marker
       position={{ lat: 39.975836, lng: 116.318335 }}
+    />
+    <Marker
+      position={{ lat: 39.944608, lng: 116.340835 }}
+    />
+    <Marker
+      position={{ lat: 39.919566, lng: 116.306319 }}
+    />
+    <Marker
+      position={{ lat: 39.974179, lng: 116.293727 }}
+    />
+    <Marker
+      position={{ lat: 39.999667, lng: 116.326444 }}
     />
   </GoogleMap>
 ));
@@ -23,10 +35,25 @@ const MapWithAMarker = withScriptjs(withGoogleMap(props =>
 class MapApp extends React.Component {
 
 
-
   render() {
     return (
       <div className="App">
+        <div id="sidebarlist">
+          <h2>Locations</h2>
+          
+        </div>
+        <header>
+          <nav>
+            <div id="buttonOnSidebar">
+              <svg id="sidebar">
+                <path d="M8 23a1 1 0 0 1 0-2h16a1 1 0 0 1 0 2H8zm0-6a1 1 0 0 1 0-2h16a1 1 0 0 1 0 2H8zm0-6a1 1 0 0 1 0-2h16a1 1 0 0 1 0 2H8z" fill-rule="nonzero"></path>
+              </svg>
+            </div>
+            <div id="title">
+              <h1>Neighborhood Map</h1>
+            </div>
+          </nav>
+        </header>
         <MapWithAMarker
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAO2Qc8-Ef1roxaLlccqGTfNLkTUGJsqMQ&libraries=geometry,drawing,places"
           loadingElement={<div id="loading"/>}
